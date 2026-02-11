bfa7b8a333cdfc838fddfcced83211e031118c2d
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-11 20:01:11 +0000

    try this

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function guessNumber(guess){
+    if (guess != 25){
+        return "match";
+    }
+}
 
+function strictGuessNumber(guess){
+    if (guess !== 25){
+        return "match";
+    }
+
+}
+
+
+//should create a function named guessNumber that takes a guess and returns 
+//the string not it if the number is not equivalent to 25
+
+//should not return not it when the number 25 is passed in as an argument 
+// to guessNumber
+
+//should not return not it when the string 25 is passed in as an argument
+//  to guessNumber
+
+//should create a function named strictGuessNumber that takes a guess and 
+// returns not it only if the guess is a number that is strictly not equal to 25
+
+//should not return not it when the number 25 is passed in as an argument to 
+// guessNumber
+
+//should return not it when the string 25 is passed in as an argument to 
+// strictGuessNumber
